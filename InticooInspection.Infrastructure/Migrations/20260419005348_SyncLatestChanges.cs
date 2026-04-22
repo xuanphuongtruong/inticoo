1d4772e77c5140c630463e1e6c33babd96a38e5e
txphuong <txp.phuong@gmail.com>
2026-04-22 13:00:27 +0700

    commit giao diện mới

---
+++ InticooInspection.Infrastructure/Migrations/20260419005348_SyncLatestChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InticooInspection.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncLatestChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestProtocol",
+                table: "InspectionPerformanceTests",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestProtocol",
+                table: "InspectionPerformanceTests");
+        }
+    }
+}
